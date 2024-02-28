89fbdb9b93a15e5334663d5da516af56f1e3c139
skyleroriordan <skyler.oriordan@mymail.champlain.edu>
2024-02-28 21:35:19 +0000

    modified:   secure-shh.sh

--- SYS265/linux/centos7/secure-shh.sh
+++ SYS265/linux/centos7/secure-shh.sh
@@ -4,9 +4,9 @@
 #adds a public key from the local repo or cureld from the remote repo
 #removes roots ability to shh in
 echo "ALL YOUR CODE GOES HERE"
-sudo useradd -m -d /home/testuser12 -s /bin/bash testuser12
-sudo mkdir /home/testuser12/.ssh
-sudo cp SYS265/linux/public-keys/id_rsa.pub /home/testuser12.ssh/authorized_keys
-sudo chmod 700 /home/testuser12/.ssh
-sudo chmod 600 /home/testuser12/.ssh/authorized_keys
-sudo chown -R testuser12:testuser12 /home/testuser12/.ssh
+sudo useradd -m -d /home/"$1" -s /bin/bash "$1"
+sudo mkdir /home/"$1"/.ssh
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/"$1"/.ssh/authorized_keys
+sudo chmod 700 /home/"$1"/.ssh
+sudo chmod 600 /home/"$1"/.ssh/authorized_keys
+sudo chown -R "$1":"$1" /home/"$1"/.ssh
