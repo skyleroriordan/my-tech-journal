6adb3410287a9e773e8746e168d6887ec100f32c
skyleroriordan <skyler.oriordan@mymail.champlain.edu>
2024-02-28 00:19:49 -0500

    new file:   centos7/secure-shh.sh

---
+++ SYS265/linux/centos7/secure-shh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author skyleroriordan
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or cureld from the remote repo
+#removes roots ability to shh in
+echo "ALL YOUR CODE GOES HERE"
