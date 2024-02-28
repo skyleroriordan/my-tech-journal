5106d37ffdf22e1bc3ce9784a2d7f7caa4040c55
skyleroriordan <113953398+skyleroriordan@users.noreply.github.com>
2024-02-28 00:01:51 -0500

    Delete SYS265/linux/centos7 directory

--- SYS265/linux/centos7/secure-ssh.sh
+++
@@ -1,6 +0,0 @@
-#secure-ssh.sh
-#author skyleroriordan
-#creates a new ssh user $1 parameter
-#adds a public key from the local repo or curled from the remote repo
-#removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
