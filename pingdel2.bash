4cc75565fd3c35e997817dcb2fdb54e66ecd0274
skyleroriordan <113953398+skyleroriordan@users.noreply.github.com>
2024-09-08 21:01:35 +0100

    Delete pingdel2.bash

--- pingdel2.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-> sweep.txt
-
-for ip in $seq 2 50); do
-          if ping -c 1 -W 10.0.5.$ip | grep -q "bytes from"; then
-                  echo "10.0.5.$ip" >> sweep.txt
-          fi
-done
